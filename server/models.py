97f816b114b576a7e2f99cf78189c27477332f46
Moringa School <sylvesterkonana005@gmail.com>
2026-04-17 21:24:10 +0300

    setting up relationship

---
+++ server/models.py
@@ -0,0 +1,28 @@
+from extensions import db, bcrypt
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    entries = db.relationship("JournalEntry", backref="user", lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+
+class JournalEntry(db.Model):
+    __tablename__ = "journal_entries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
